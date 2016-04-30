88eca2d257d38195332d33dc8a704d3e4240bf46
Windows 8.1 <HebaHashem>
2016-04-30 22:04:47 +0200

    adding markes

--- src/proj1/project1.java
+++ src/proj1/project1.java
@@ -41,6 +41,31 @@ public class project1  extends Application{
 			     for(int i =2 ;i<=4 ;i++)
 			    	 for(int j=2;j>=0;j--)
 			    		g.add(arr[k--], j, i);
+			     
+			      // adding markes
+			      
+			      
+		      Button []arr2= new Button[10];
+		      for(int i =0; i<10 ;i++){	
+		    	  arr2[i]= new Button();
+		    	  arr2[i].setPrefSize(50,40);
+		      } 
+		      int a=0;
+			     for(int i =2 ;i<=4 ;i++){
+			    	 for(int j=3;j<=5;j++)
+			    	g.add(arr2[a++], j, i); 
+			    	 
+			    	 
+			     }
+			     arr2[0].setText("/");
+			     arr2[1].setText("DEL");         
+			     arr2[2].setText("AC");       
+			     arr2[3].setText("*");
+			     arr2[4].setText("(");
+			     arr2[5].setText(")");
+			     arr2[6].setText("-");
+			     arr2[7].setText("x^2");       
+			     arr2[8].setText("Sqrt"); 
 			       g.setHgap(3);
                    g.setVgap(3);		
 		st.setScene(new Scene(g,300,200));
@@ -84,5 +109,55 @@ public class project1  extends Application{
 		text.setText((text.getText()+arr[9].getText()));
 		
 		});
+	arr2[0].setOnAction(e->{
+		text.setText((text.getText()+" " +arr2[0].getText()+" "));
+	
+	});
+
+	arr2[1].setOnAction(e->{
+		String remind= text.getText();
+		
+		      if(Character.isDigit(remind.charAt(remind.length()-1)))
+				text.setText(remind.substring(0, remind.length()-1));
+		      
+		      else
+		    	  text.setText(remind.substring(0, remind.length()-3));  
+	
+	});		
+	arr2[2].setOnAction(e->{
+		text.setText("");
+	
+	});
+
+	arr2[3].setOnAction(e->{
+		text.setText((text.getText()+" "+arr2[3].getText()+" "));
+	
+	});
+
+	arr2[4].setOnAction(e->{
+		text.setText((text.getText()+arr2[4].getText())+" ");
+	
+	});
+
+	arr2[5].setOnAction(e->{
+		text.setText((text.getText()+" " +arr2[5].getText()));
+	
+	});
+
+	arr2[6].setOnAction(e->{
+		text.setText((text.getText()+" " +arr2[6].getText()+" "));
+	
+	});
+
+	arr2[7].setOnAction(e->{
+		text.setText((text.getText()+" " +"^ 2"));
+	
+	});
+
+	arr2[8].setOnAction(e->{
+		text.setText((text.getText()+" " +arr2[8].getText()+" "));
+	
+	});
+	
 	}}
 	
